ab857192afb0e2098173e31dfefe1ef7597df6ba
Bran Lang <branislavlang@gmail.com>
2026-01-26 22:01:21 +0100

    Update select-native.tsx

--- frontend/src/components/ui/select-native.tsx
+++ frontend/src/components/ui/select-native.tsx
@@ -11,7 +11,7 @@ const NativeSelect = React.forwardRef<HTMLSelectElement, NativeSelectProps>(
             <div className="relative">
                 <select
                     className={cn(
-                        "flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 appearance-none",
+                        "flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-background px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 appearance-none text-foreground [&>option]:bg-background [&>option]:text-foreground",
                         className
                     )}
                     ref={ref}
